6f5381108f634ee2d4453d9e38676920b7c58c40
wangchuang <wangchuang227@163.com>
2021-08-30 15:04:35 +0800

    1

--- main.go
+++ main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
-	"github.com/olivere/elastic/v7",
+	"context"
+	"flag"
 	"fmt"
+	"time"
+
+	e "github.com/olivere/elastic/v7"
 )
 
+func main() {
 
-func main(){
+	index := flag.String("index", "", "索引名")
+	url := flag.String("url", "", "es地址")
+	deletetime := flag.String("deletetime", "-240h", "删除前多少天的数据,默认10天")
+	flag.Parse()
 
-	client, err := elastic.NewClient(elastic.SetURL("http://192.168.2.10:9201"))
+	client, err := e.NewClient(e.SetSniff(false), e.SetURL(*url))
 	if err != nil {
 		// Handle error
-		fmt.printf("连接错误")
+		fmt.Println(err)
 	}
 
+	now := time.Now()
+	d, _ := time.ParseDuration(*deletetime)
+	deleteTime := now.Add(d).Format("2006-01-02")
 
+	query := e.Query(e.NewRangeQuery("timestamp").Lte(deleteTime))
 
+	fmt.Print(query)
+
+	resule, err := client.DeleteByQuery().Index(*index).Query(query).Do(context.Background())
+	if err != nil {
+		// Handle error
+		fmt.Println(err)
+	} else {
+		fmt.Println(resule)
+	}
 
 }
